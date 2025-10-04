999f4a85e0f8848bea81d4d48bb16e998d35f4e0
Eren-Krtl <erenkurtluk@gmail.com>
2025-10-04 12:53:50 +0300

    Add files via upload

--- NasaTest/script.js
+++ NasaTest/script.js
@@ -1,7 +1,9 @@
-// Change text by the scroll
-const title = document.getElementById("title");
+// zoom when scrolled
+const picture = document.getElementById("pictureBig")
 
-window.addEventListener('wheel', function(event) {
+window.addEventListener('wheel', function(event)
+{
+  event.preventDefault();
   let zoomMultiplier = event.deltaY / 108;
   title.innerText = `Scroll amount ${zoomMultiplier}`;
-});
+},{passive: false});
