4819be0fc5aeafc950c2e19edb23a1c95f547301
Eren-Krtl <erenkurtluk@gmail.com>
2025-10-05 12:44:53 +0300

    Delete NasaTest directory

--- NasaTest/script.js
+++
@@ -1,9 +0,0 @@
-// zoom when scrolled
-const picture = document.getElementById("pictureBig")
-
-window.addEventListener('wheel', function(event)
-{
-  event.preventDefault();
-  let zoomMultiplier = event.deltaY / 108;
-  title.innerText = `Scroll amount ${zoomMultiplier}`;
-},{passive: false});
